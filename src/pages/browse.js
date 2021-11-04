c77dd07e22648f8e421fc891e6083aab20ce1363
Samuel nye <samuelnyevr@gmail.com>
2021-11-04 01:34:00 -0700

    added filter icon

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -4,18 +4,76 @@ import axios from "axios"
 
 export default function Browse() {
     const [products, setProducts] = useState([]);
+    const [showFilter, setShowFilter] = useState(true);
+    const [filterOptions, setFilterOptions] = useState({});
 
     useEffect(() => {
         axios.get("http://localhost:3001/products")
         .then((res) => {
             setProducts(res.data);
+        });
+        setFilterOptions({
+            WomenPlainTee: false,
+            KidsPlainTee: false,
+            MenGraphicTee: false,
+            WomenGraphic: false,
+            StripeTee: false
         })
-        ;
+
     }, []) 
+
+    const handleFilterIconClick = () => {
+        setShowFilter(!showFilter);
+    }
+
+    const handleFilterListClick = (event) => {
+        if(event.target.classList.contains("WomenPlainTee")) {
+            setFilterOptions({...filterOptions, WomenPlainTee: !filterOptions.WomenPlainTee});
+        }
+        else if(event.target.classList.contains("KidsPlainTee")) {
+            setFilterOptions({...filterOptions, KidsPlainTee: !filterOptions.KidsPlainTee});
+        }
+        else if(event.target.classList.contains("MenGraphicTee")) {
+            setFilterOptions({...filterOptions, MenGraphicTee: !filterOptions.MenGraphicTee});
+        }
+        else if(event.target.classList.contains("WomenGraphic")) {
+            setFilterOptions({...filterOptions, WomenGraphic: !filterOptions.WomenGraphic});
+        }
+        else if(event.target.classList.contains("StripeTee")) {
+            setFilterOptions({...filterOptions, StripeTee: !filterOptions.StripeTee});
+        }
+    }
 
     return (
         <div className="Pages">
-            <h1 className="Header">Browse</h1>
+            <section className="BrowseHeader Header">
+                <h1 className="">Browse</h1>
+                {showFilter ?
+                <section className="Filter" onClick={handleFilterIconClick}>
+                    <h2>Filter</h2>
+                    <div className="FilterIcon">
+                        <div className="Icon Line1"></div>
+                        <div className="Icon Line2"></div>
+                        <div className="Icon Line3"></div>
+                        <div className="Icon Line4"></div>
+                    </div>
+                </section>
+                : 
+                <section className="Filter Modal">
+                    <div className="FilterIcon"  onClick={handleFilterIconClick}>
+                        <div className="Icon Cross1"></div>
+                        <div className="Icon Cross2"></div>
+                    </div>
+                    <div className="FilterList">
+                        <p className={`WomenPlainTee ${filterOptions.WomenPlainTee ? "FilterListActive" : ""}`} onClick={handleFilterListClick}>Women Plain Tee</p>
+                        <p className={`KidsPlainTee ${filterOptions.KidsPlainTee ? "FilterListActive" : ""}`} onClick={handleFilterListClick}>Kid's Plain Tee</p>
+                        <p className={`MenGraphicTee ${filterOptions.MenGraphicTee ? "FilterListActive" : ""}`} onClick={handleFilterListClick}>Men Graphic Tee</p>
+                        <p className={`WomenGraphic ${filterOptions.WomenGraphic ? "FilterListActive" : ""}`} onClick={handleFilterListClick}>Women Graphic</p>
+                        <p className={`StripeTee ${filterOptions.StripeTee ? "FilterListActive" : ""}`} onClick={handleFilterListClick}>Stripe Tee</p>
+                    </div>
+                </section>
+                }
+            </section>
             <section className="Browse">
             {products.map((value, key) => {
                 return (
